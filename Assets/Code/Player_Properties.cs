e93a98ef0010d23ea5554125d9f2e37a5b2e8bd5
MaskDev <118457543+MaskX377@users.noreply.github.com>
2024-08-19 17:33:50 +0500

    Player movements with rotation and without animations

--- Assets/Code/Player_Properties.cs
+++ Assets/Code/Player_Properties.cs
@@ -1,12 +1,11 @@
+
 using System.Collections;
 using System.Collections.Generic;
-using System.Data.SqlTypes;
 using UnityEngine;
 
 public class Player_Properties : MonoBehaviour
 {
     Rigidbody rb;
-
     Animator anim;
 
     public Transform Cam;
@@ -35,7 +34,6 @@ public class Player_Properties : MonoBehaviour
         return Physics.CheckSphere(GroundCheck.position, groundCheckRadius, groundLayer);
     }
 
-    // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody>();
@@ -43,17 +41,16 @@ public class Player_Properties : MonoBehaviour
         Cursor.lockState = CursorLockMode.Locked;
     }
 
-    // Update is called once per frame
     void Update()
     {
-        //if (Input.GetKey(KeyCode.LeftShift))
-        //{
-        //    speed = runSpeed;
-        //}
-        //else
-        //{
-        //    speed = walkSpeed;
-        //}
+        if (Input.GetKey(KeyCode.LeftShift))
+        {
+            speed = runSpeed;
+        }
+        else
+        {
+            speed = walkSpeed;
+        }
 
         if (gameObject != null)
         {
@@ -83,36 +80,14 @@ public class Player_Properties : MonoBehaviour
             transform.rotation = Quaternion.Euler(0, angle, 0);
 
             Vector3 moveDir = Quaternion.Euler(0, targetAngle, 0) * Vector3.forward;
-
-            rb.velocity = moveDir.normalized * speed * Time.fixedDeltaTime;
+            movement = moveDir.normalized * speed;
+        }
+        else
+        {
+            movement = Vector3.zero;
         }
 
-        //Vector3 move = Vector3.zero;
-
-        //if (Input.GetKey(KeyCode.W))
-        //{
-        //    move += Vector3.forward * speed * Time.fixedDeltaTime;
-        //    transform.rotation = Quaternion.Euler(0, Hips.rotation.x + 0, 0);
-        //}
-        //if (Input.GetKey(KeyCode.S))
-        //{
-        //    move += -Vector3.forward * speed * Time.fixedDeltaTime;
-        //    transform.rotation = Quaternion.Euler(0, Hips.rotation.x + 180, 0);
-        //}
-        //if (Input.GetKey(KeyCode.A))
-        //{
-        //    move += -Vector3.right * speed * Time.fixedDeltaTime;
-        //    transform.rotation = Quaternion.Euler(0, Hips.rotation.x - 90, 0);
-        //}
-        //if (Input.GetKey(KeyCode.D))
-        //{
-        //    move += Vector3.right * speed * Time.fixedDeltaTime;
-        //    transform.rotation = Quaternion.Euler(0, Hips.rotation.x + 90, 0);
-        //}
-
-        //movement = move;
-
-        //rb.velocity = new Vector3(move.x, rb.velocity.y, move.z);
+        rb.velocity = new Vector3(movement.x, rb.velocity.y, movement.z);
     }
 
     void Jump()
@@ -125,7 +100,7 @@ public class Player_Properties : MonoBehaviour
         {
             numberOfJumpsLeft--;
             rb.velocity = new Vector3(rb.velocity.x, 0f, rb.velocity.z); // Reset vertical velocity
-            rb.velocity = transform.up * jumpForce;
+            rb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
         }
     }
 }
